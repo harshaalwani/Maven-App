d2581b7f0fc7f73944c071c6b2d9181c9dd24098
harshaalwani8@gmail.com <harshaalwani8@gmail.com>
2023-04-26 17:18:14 +0000

    AWS-26-04-2023

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! from Aws" );
+        System.out.println( "Hello World! from Aws-26-04-2022" );
     }
 }
